656b95c9480478bf42e3e22b41d7a5872f237c77
ZhekaGrushevskiy <grushevskiy.yevgeniy@mail.ru>
2021-12-20 16:33:40 +0300

    sdfsdf

---
+++ Js/usefulFunction.js
@@ -0,0 +1,21 @@
+// Получение словоформы для числа
+
+// function getNumWord(num,word1,word2,word5) {
+//     var dd=num%100;
+//     if ( (dd>=11) && (dd<=19) )
+//         return word5;
+//     var d=num%10;
+//     if ( d==1 )
+//         return word1;
+//     if ( (d>=2) && (d<=4) )
+//         return word2;
+//     return word5;
+// }
+
+// function test() {
+//     var applesS=prompt('Сколько у вас яблок?');
+//     var apples=parseInt(applesS);
+//     alert( 'У вас ' + apples + ' ' + getNumWord(apples,'яблоко','яблока','яблок') + '!' );
+// }
+
+///////////////////////////////////
