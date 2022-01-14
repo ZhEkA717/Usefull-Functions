b6e8c3a2db9e4913a0a21b58595554b8701450b5
ZhekaGrushevskiy <grushevskiy.yevgeniy@mail.ru>
2022-01-14 11:31:55 +0300

    position circle

--- Js/usefulFunction.js
+++ Js/usefulFunction.js
@@ -49,23 +49,48 @@
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Для вычисления положения элемента относительно левого верхнего угла окна браузера 
-// (не относительно начала документа!) можно использовать метод элемента getBoundingClientRect,
-// который возвращает хэш со свойствами top, left, right, bottom, а в современных браузерах — 
-// также и свойствами width и height.
-var elem = document.getElementById('proba');
-var pos = elem.getBoundingClientRect();
-// теперь pos - хэш вида {left:XXX, top:XXX, right:XXX, bottom:XXX}
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // Для вычисления положения элемента относительно левого верхнего угла окна браузера 
+// // (не относительно начала документа!) можно использовать метод элемента getBoundingClientRect,
+// // который возвращает хэш со свойствами top, left, right, bottom, а в современных браузерах — 
+// // также и свойствами width и height.
+// var elem = document.getElementById('proba');
+// var pos = elem.getBoundingClientRect();
+// // теперь pos - хэш вида {left:XXX, top:XXX, right:XXX, bottom:XXX}
 
-// Позицию элемента относительно левого верхнего угла страницы можно получить, 
-// добавив к результату getBoundingClientRect текущую прокрутку окна браузера:
-function getElementPos(elem) {
-    var bbox=elem.getBoundingClientRect();
-    return {
-      left: bbox.left+window.pageXOffset,
-      top: bbox.top+window.pageYOffset
-    };
-  }
+// // Позицию элемента относительно левого верхнего угла страницы можно получить, 
+// // добавив к результату getBoundingClientRect текущую прокрутку окна браузера:
+// function getElementPos(elem) {
+//     var bbox=elem.getBoundingClientRect();
+//     return {
+//       left: bbox.left+window.pageXOffset,
+//       top: bbox.top+window.pageYOffset
+//     };
+//   }
 
-  console.log(pos);
+//   console.log(pos);
+//   //////////////////////////////////////////////////////////////////////////////////
+
+//   //////////////////////////////////////////////////////////////////////////////////
+// позициоируем землю относительно солнце на заданное расстояние и угол
+function pos( ) {
+
+  var red=document.getElementById('RED');
+  var green=document.getElementById('GREEN');
+
+  var form=document.forms.FPos;
+  var radius=parseFloat(form.elements.Radius.value);
+  var angle=parseFloat(form.elements.Angle.value)/180*Math.PI;
+
+  var redCenterX=red.offsetLeft+red.offsetWidth/2;
+  var redCenterY=red.offsetTop+red.offsetHeight/2;
+
+  var greenCenterX=redCenterX+radius*Math.sin(angle);
+  var greenCenterY=redCenterY-radius*Math.cos(angle);
+
+  green.style.left=Math.round(greenCenterX-green.offsetWidth/2)+'px';
+  green.style.top=Math.round(greenCenterY-green.offsetHeight/2)+'px';
+}
+
+pos();
+//   //////////////////////////////////////////////////////////////////////////////////
